319d66351efb0d70a9902eba413afd36542c5e83
manjunathan <120540304+manjunathan1@users.noreply.github.com>
2023-04-01 18:53:55 +0530

    Add files via upload

---
+++ project.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class project{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter your born year");
+int name=sc.nextInt();
+int name2=2023;
+int name3=name2-name;
+System.out.println(name3);
+
+
+}
+}
